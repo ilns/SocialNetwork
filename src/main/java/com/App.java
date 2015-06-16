12e0f72b0c92db958f21af21565f698e8e7ed477
ILNS <illinois93@mail.ru>
2015-06-16 20:10:39 +0300

    test

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -8,5 +8,6 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class App {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
+        // develop
     }
 }
