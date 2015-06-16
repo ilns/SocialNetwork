c06812b0580b95948edd46c713eeef27e4ea4853
ILNS <illinois93@mail.ru>
2015-06-16 20:06:58 +0300

    fix

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -1,7 +1,5 @@
 package com;
 
-import com.Service.interfaces.JavaContactService;
-import com.model.Contact;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
@@ -10,11 +8,5 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class App {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
-        ContactService javaContactService = context.getBean(JavaContactService.class);
-        ContactDaoImpl contactDao = context.getBean(ContactDaoImpl.class);
-        Contact contact = context.getBean(Contact.class);
-
-        System.out.println(javaContactService.toString());
-        System.out.println("___________________________________________________");
     }
 }
