adb69f4c8b96e9cb89d12da7f019cc9291bd1306
DSkalenko <d.skalenko@gmail.com>
2015-06-25 17:25:48 +0200

    test commit

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -7,6 +7,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class App {
     public static void main(String[] args) {
+
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
     }
 }
